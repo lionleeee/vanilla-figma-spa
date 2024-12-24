7f81a49cf74583fe1d6189dca3a40ca037b176c3
lionleee <lee97hun3@gmail.com>
2024-12-24 15:53:42 +0900

    build: 빌드 관련 기본 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,20 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+
+  optimizeDeps: {
+    include: ['./src/core/router/Router.js'],
+  },
+  build: {
+    sourcemap: true,
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['./src/index.js', './src/core/router/Router.js'],
+        },
+      },
+    },
+  },
 });
