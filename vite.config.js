48670f4f12f1955f73cb83731c831650f4d7d158
lionleee <lee97hun3@gmail.com>
2024-12-24 16:03:07 +0900

    build : dev, prod 환경 분리

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-
-  optimizeDeps: {
-    include: ['./src/core/router/Router.js'],
-  },
-  build: {
-    sourcemap: true,
-    cssCodeSplit: true,
-    chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['./src/index.js', './src/core/router/Router.js'],
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  return {
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    define: {
+      __DEV__: isDev,
+    },
+    optimizeDeps: {
+      include: ['./src/core/router/Router.js'],
+    },
+    build: {
+      minify: isDev,
+      minify: !isDev,
+      sourcemap: isDev,
+      chunkSizeWarningLimit: 500,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['./src/index.js', './src/core/router/Router.js'],
+          },
         },
       },
     },
-  },
+  };
 });
